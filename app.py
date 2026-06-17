cd68fc08b5678a0bf89ddf65f9e5c677fdb5d25e
vann-231007 <vl1045644@gmail.com>
2026-06-17 11:01:28 +0700

    Update app title to reflect new author

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.image("logo.jpg")
 # Tiêu đề app
-st.title("APP TÍNH TIỀN GỬI TIẾT KIỆM_ĐỀ TÀI 2_TS. VŨ ĐỨC BÌNH")
+st.title("APP TÍNH TIỀN GỬI TIẾT KIỆM_ĐỀ TÀI 2_TS. LÊ THỊ THẢO VÂN")
 
 # Nhập dữ liệu
 C = st.number_input(
